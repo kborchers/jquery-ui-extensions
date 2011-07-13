ef72b39c8fa341b6cbf2154862b97b760c229c21
kborchers <k_borchers@yahoo.com>
2011-07-13 12:43:39 -0500

    InputMenu: Empty function to test

---
+++ menu/jquery.ui.menu.inputmenu.js
@@ -0,0 +1,13 @@
+/*
+ * jQuery UI Input Menu Extension
+ *
+ * Copyright 2010, Kris Borchers
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * http://github.com/kborchers/jquery-ui-extensions
+ */
+(function( $ ) {
+
+
+
+})( jQuery );
